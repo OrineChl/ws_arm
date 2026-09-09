cde1df3d24779f94ba084895d1704977259c0a53
OrineChl <orinechl3236459088@gmail.com>
2026-09-09 11:30:34 +0800

    测试restore

--- src/custom_3dof_description/launch/display.launch.py
+++ src/custom_3dof_description/launch/display.launch.py
@@ -25,3 +25,6 @@ def generate_launch_description():
         Node(package='rviz2', executable='rviz2',
              arguments=['-d', LaunchConfiguration('rviz_config')], output='screen'),
     ])
+
+
+
